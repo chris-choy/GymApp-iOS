629039e94fde442a770adb5ed0fdb42f4a8747f8
Chris <chrischoy_95@hotmail.com>
2020-04-16 09:57:00 +0800

    Initial Commit

---
+++ GymApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  GymApp
+//
+//  Created by Chris on 2020/4/16.
+//  Copyright © 2020 Chris. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
